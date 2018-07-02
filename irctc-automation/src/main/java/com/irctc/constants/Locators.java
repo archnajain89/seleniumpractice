8cdb8656fb768ee7d4c602e3b843de0a6130a909
arrchnnajaiin <archnajain89@gmail.com>
2018-07-02 18:48:24 +0530

    added locators for login page

---
+++ irctc-automation/src/main/java/com/irctc/constants/Locators.java
@@ -0,0 +1,9 @@
+package com.irctc.constants;
+
+public class Locators {
+
+	public static final String USER_ID = "//*[@id='identifierId']";
+	public static final String PASS = "//*[@name='password']";
+	public static final String NEXT_BUTTON = "//*[@class='RveJvd snByac']";
+	public static final String PASSWORD_NEXT_BUTTON="//*[@id='passwordNext']/div[2]";
+}
