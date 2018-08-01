7be2aea03790fcef04c623a7b996bb2a26a62bb4
arrchnnajaiin <archnajain89@gmail.com>
2018-08-01 16:30:58 +0530

    adding extent report jar in pom.xml

--- irctc-automation/src/test/java/com/gmail/mail/SendMail.java
+++ irctc-automation/src/test/java/com/gmail/mail/SendMail.java
@@ -1,13 +1,19 @@
 package com.gmail.mail;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import com.gmail.constants.Locators;
 import com.gmail.constants.Standard;
 import com.gmail.login.Login;
@@ -17,7 +23,7 @@ public class SendMail extends Login {
 	@Test(priority = 2, groups = { "T1" })
 	public void composeMailBtnClick() {
 		// driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		//wait = new WebDriverWait(driver, 20);
+		// wait = new WebDriverWait(driver, 20);
 		wait.until(ExpectedConditions.visibilityOfElementLocated((By.xpath(Locators.COMPOSE_BTN))));
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath(Locators.COMPOSE_BTN)));
 		driver.findElement(By.xpath(Locators.COMPOSE_BTN)).click();
@@ -46,14 +52,27 @@ public class SendMail extends Login {
 	}
 
 	@Test(priority = 5, groups = { "T1" })
-	public void sendMail() throws InterruptedException {
-		//driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	public void sendMail() throws InterruptedException, IOException {
+		// driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		Thread.sleep(5000);
 		driver.findElement(By.xpath(Locators.SEND_BTN)).click();
 		wait.until(ExpectedConditions.visibilityOfElementLocated((By.xpath(Locators.SENT_MSG))));
-		WebElement msg=driver.findElement(By.xpath(Locators.SENT_MSG));
-		String actualMessage=msg.getText();
-		Assert.assertEquals(actualMessage,Standard.EXPECTED_MSG);
+		WebElement msg = driver.findElement(By.xpath(Locators.SENT_MSG));
+		String actualMessage = msg.getText();
+		Assert.assertEquals("sdfsdfsdfs", Standard.EXPECTED_MSG);
+		try {
+			Assert.assertEquals("sdfsdfsdfs", Standard.EXPECTED_MSG);
+		} catch (AssertionError e) {
+			// Take screenshot if Assertion fails.
+			String filename = "failed";
+			DateFormat dateFormat = new SimpleDateFormat("dd-mm-yyyy h-m-s");
+			Date date = new Date();
+			File f1 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			File f2 = new File("D:\\Screenshot\\" + filename + "-" + dateFormat.format(date) + ".png");
+			FileUtils.copyFile(f1, f2);
+
+		}
+
 	}
 
 }
--- irctc-automation/src/test/java/com/gmail/screenshots/Screenshot.java
+++ irctc-automation/src/test/java/com/gmail/screenshots/Screenshot.java
@@ -1,7 +1,13 @@
 package com.gmail.screenshots;
 
-public class Screenshot {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+import com.gmail.base.StartBrowser;
+import com.gmail.constants.Standard;
+
+public class Screenshot {
+	public WebDriver driver;
 	
 	
 	
