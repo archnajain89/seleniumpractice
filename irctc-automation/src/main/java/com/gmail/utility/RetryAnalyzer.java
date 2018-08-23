f875b11ddd4222722ee320cd43cfb09d2a8cc297
arrchnnajaiin <archnajain89@gmail.com>
2018-08-23 18:27:54 +0530

    impemented Retry Analyzer code

---
+++ irctc-automation/src/main/java/com/gmail/utility/RetryAnalyzer.java
@@ -0,0 +1,28 @@
+package com.gmail.utility;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer {
+	private int retryCount = 0;
+	private static int maxRetryCount = 2;
+
+	public boolean retry(ITestResult result) {
+		// TODO Auto-generated method stub
+		 if (!result.isSuccess()) {                      //Check if test not succeed
+	            if (retryCount < maxRetryCount) {                            //Check if maxtry count is reached
+	            	retryCount++;                                     //Increase the maxTry count by 1
+	            	System.out.println("retryCount"+retryCount);
+	                result.setStatus(ITestResult.FAILURE);  //Mark test as failed
+	                return true;                                 //Tells TestNG to re-run the test
+	            } else {
+	            	result.setStatus(ITestResult.FAILURE);  //If maxCount reached,test marked as failed
+	            }
+	        } else {
+	        	result.setStatus(ITestResult.SUCCESS);      //If test passes, TestNG marks it as passed
+	        }
+	        return false;
+
+	}
+
+}
